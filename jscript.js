6448594b5e1c58fc8bda670ca77ab3a6546f1e0f
majo <maria.riofrio.mr@gmail.com>
2020-03-28 20:04:11 -0400

    current work

--- jscript.js
+++ jscript.js
@@ -1,14 +1,20 @@
 //document.ready?
 document.addEventListener("DOMContentLoaded", function(){
-
+// added elements 
     var $startEl = document.querySelector("#start");
     var $quiz = document.querySelector("#quiz");
     var $results = document.querySelector("#results");
     var $timer = document.querySelector("#timer");
     var $submitEl = document.querySelector("#submit");
     var $initialEl = document.querySelector("#initial");
-    // var $initialInput = document.querySelector("#initial");
+    var $formEl = document.querySelector("#form");
+
     
+
+    $formEl.style.display = "none";
+
+// added start and submit buttons 
+
     $startEl.addEventListener("click", function(event){
         event.preventDefault()
         console.log("we clicked it")
@@ -24,10 +30,10 @@ document.addEventListener("DOMContentLoaded", function(){
         $initialEl.innerText = 
         showEnding()
     })    
-
-    var secondsLeft = 20;
+// timer 
+    var secondsLeft = 40;
     setTime()
-
+// questions and answers for student 
     var currentQuestion = 0
     let questions = [
         {
@@ -45,9 +51,12 @@ document.addEventListener("DOMContentLoaded", function(){
             answers: ["alert", "array", "DOM"]
         }
     ]
+    // 
     function showEnding(){
+        $results.style.display = "none";
         $quiz.innerHTML= `<h> Results </h2>`
         clearInterval(timerInterval);
+        $formEl.style.display = "block";
 
         let unparsed=localStorage.getItem("highscored")
         console.log(unparsed)
@@ -63,7 +72,10 @@ document.addEventListener("DOMContentLoaded", function(){
        console.log(toStorage)
         localStorage.setItem("highscored", JSON.stringify(toStorage))
     }
+
+    // functions with questions
     function showQuestion(){
+    
         let questionObject = questions[currentQuestion]
         const $newDiv= document.createElement("div")
         const $thisQuestion = document.createElement("h4")//someday talk about pointers
@@ -73,15 +85,17 @@ document.addEventListener("DOMContentLoaded", function(){
         console.log($newDiv)
         
         questionObject.answers.forEach(answer => {
+            
             const $thisanswer = document.createElement("button")
             $thisanswer.innerText = answer
             $thisanswer.addEventListener("click", function(event){
                 event.preventDefault()
                 let selection= this.innerText
                 if(selection==questionObject.rightAnswer){
-                    $results.innerText= "great job"
+                    $results.innerText= "Great Job"
                 }else {  
-                    $results.innerText= "keep at it"
+                    $results.innerText= "Wrong Answer"
+
         
                 }
                 $quiz.innerHTML = ''
@@ -101,13 +115,13 @@ document.addEventListener("DOMContentLoaded", function(){
         $quiz.appendChild($newDiv)
         
     }
-    
+    // timer function 
     var timerInterval
     function setTime() {
        timerInterval = setInterval(function() {
         secondsLeft--;
         $timer.textContent = secondsLeft;
-    
+        
         if(secondsLeft === 0) {
           showEnding()
         }
@@ -115,15 +129,16 @@ document.addEventListener("DOMContentLoaded", function(){
       }, 1000);
     }
 
+    // function myFunction() {
+    //     document.getElementById("$timer").style.alignItems = "flex-start";
+    //   }
     
 
 })
     
     //add names to high scores 
     //add scores 
-    // create element
-   // set that element's innertext or innerHTML´
-   // put that element on the page
+    
 
    // add timer 
 
